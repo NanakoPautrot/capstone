a10b694202290ec46de008e8a7ab883681349148
Nanako Pautrot <nanakonishiguchi@gmail.com>
2018-03-30 20:29:45 +0800

    updated

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'nookDatabase',
+      database: '',
       user:     '',
       password: ''
     }
