d71fe36c693659f8782e0b1db094d4d33489708b
Nanako Pautrot <nanakonishiguchi@gmail.com>
2018-04-12 01:14:24 +0800

    google maps feature added

--- frontend/src/app/restaurants/restaurants.component.ts
+++ frontend/src/app/restaurants/restaurants.component.ts
@@ -20,20 +20,67 @@ export class RestaurantsComponent implements OnInit {
 restaurants: any;
 district:string;
 facility?:number;
+region: string;
+lat: number;
+lng: number;
+
+options = {
+  enableHighAccuracy: false,
+  timeout: 30000,
+  maximumAge: 0
+};
+
+error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
 
   constructor(private route: ActivatedRoute, private router: Router, private restaurantService: RestaurantService) { }
 
-  //constructor(private restaurantService : RestaurantService, private route: ActivatedRoute, private router: Router, private restaurantRegionService: RestaurantRegionService) { }
- 
+  
   ngOnInit() {
-  this.getRestaurants();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+      console.log(this.lat);
+    }, this.error, this.options);
+    if (this.route.queryParams['_value'].district) {
+      this.getRestaurants(this.route.queryParams['_value'].district);
+    } else {
+      this.getRestaurants('Aberdeen');
+    }
   }
-
-  getRestaurants(): void {
-    this.restaurantService.getRestaurants('Tsim Sha Tsui',3)
-    .subscribe(res => this.restaurants=res);
+  
+    getRestaurants(district): void {
+      this.restaurantService.getRestaurants(district).subscribe((res: any) => {
+        const ref = [];
+        res = res.filter((element, idx) => {
+          if (ref.indexOf(element.id) === -1) {
+            ref.push(element.id);
+            return element;
+          }
+        });
+        this.restaurants = res;
+      });
+    }
+    openLink(path: string) {
+      console.log(path);
+    }
   }
-
-}
+  
+  
+  function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    const dLon = deg2rad(lon2 - lon1);
+    // tslint:disable-next-line:max-line-length
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return d;
+  }
+  
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
 
 
