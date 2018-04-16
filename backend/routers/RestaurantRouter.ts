ef14f1ec4d6c2a820554553b7134daeb95124c5b
Wawa <whabimana@gmail.com>
2018-04-16 13:44:51 +0800

    trying to solve facility issues by optional parameter and other changes

--- backend/routers/RestaurantRouter.ts
+++ backend/routers/RestaurantRouter.ts
@@ -65,14 +65,14 @@ export default class RestaurantRouter{
     
     getRestauByDistrict(req: express.Request, res: express.Response){
         // let district:string = req.params.district;
-        // return this.restaurantService.districtRestau(district)
-        // .then((data) =>{
-        //     res.json(data);
-        // })
-        // .catch((err: express.Errback) => {
-        //     res.status(500).json(err)
-        // });
-        console.log(req.query.id)
+         return this.restaurantService.districtRestau(req.query.district, req.query.facility,req.query.id)
+         .then((data:any) =>{
+            res.json(data);
+        })
+        .catch((err: express.Errback) => {
+            res.status(500).json(err)
+        });
+    
     }
     getRestauByFacility(req: express.Request, res: express.Response){
         let district:string = req.params.district;
--- backend/services/RestaurantService.ts
+++ backend/services/RestaurantService.ts
@@ -14,95 +14,109 @@ export default class RestaurantService {
 
     list(district: string = '') {
         return this.knex('restaurants')
-        .innerJoin('districts', 'districts.id', 'restaurants.district_id')
-        .innerJoin('restaurants_facilities', 'restaurants_facilities.restaurant_id', 'restaurants.id')
-        .join('facilities', 'facilities.id','restaurants_facilities.facility_id')
-        .select({
-            'id'               : 'restaurants.id',       
-            'name'             : 'restaurants.name',                     
-            'telephone'        : 'restaurants.telephone',              
-            'address'          : 'restaurants.address',            
-            'latitude'         : 'restaurants.latitude',               
-            'longitude'        : 'restaurants.longitude',              
-            'img_url'          : 'restaurants.img_url',            
-            'cuisine'          : 'restaurants.cuisine',            
-            'category'         : 'restaurants.category',               
-            'rating'           : 'restaurants.rating',             
-            'score_smile'      : 'restaurants.score_smile',            
-            'score_ok'         : 'restaurants.score_ok',              
-            'score_sad'        : 'restaurants.score_sad',              
-            'price_range'      : 'restaurants.price_range',            
-            'district_id'      : 'districts.id',                       
-            'district_name'    : 'districts.name',                        
-            'region_name'      : 'districts.region'                   
-        })
-        .where('districts.name', district)
+            .innerJoin('districts', 'districts.id', 'restaurants.district_id')
+            .innerJoin('restaurants_facilities', 'restaurants_facilities.restaurant_id', 'restaurants.id')
+            .join('facilities', 'facilities.id', 'restaurants_facilities.facility_id')
+            .select({
+                'id': 'restaurants.id',
+                'name': 'restaurants.name',
+                'telephone': 'restaurants.telephone',
+                'address': 'restaurants.address',
+                'latitude': 'restaurants.latitude',
+                'longitude': 'restaurants.longitude',
+                'img_url': 'restaurants.img_url',
+                'cuisine': 'restaurants.cuisine',
+                'category': 'restaurants.category',
+                'rating': 'restaurants.rating',
+                'score_smile': 'restaurants.score_smile',
+                'score_ok': 'restaurants.score_ok',
+                'score_sad': 'restaurants.score_sad',
+                'price_range': 'restaurants.price_range',
+                'district_id': 'districts.id',
+                'district_name': 'districts.name',
+                'region_name': 'districts.region'
+            })
+            .where('districts.name', district)
     }
 
 
     restaurant(id: string = '') {
         return this.knex('restaurants')
-        .select()
-        .where('id', id)
+            .select()
+            .where('id', id)
     }
 
 
     //to get restaurants by a specific selected district
-    districtRestau(district:string){
-        return this.knex('restaurants')
+    districtRestau(/*district:string*/districtName: string, facilityId: string, restaurantId: string) {
+        //return this.knex('restaurants')
+        let query = this.knex('restaurants')
             .innerJoin('districts', 'districts.id', 'restaurants.district_id')
             .innerJoin('restaurants_facilities', 'restaurants_facilities.restaurant_id', 'restaurants.id')
             .select({
-                'id'               : 'restaurants.id',       
-                'name'             : 'restaurants.name',                     
-                'telephone'        : 'restaurants.telephone',              
-                'address'          : 'restaurants.address',            
-                'latitude'         : 'restaurants.latitude',               
-                'longitude'        : 'restaurants.longitude',              
-                'img_url'          : 'restaurants.img_url',            
-                'cuisine'          : 'restaurants.cuisine',            
-                'category'         : 'restaurants.category',               
-                'rating'           : 'restaurants.rating',             
-                'score_smile'      : 'restaurants.score_smile',            
-                'score_ok'         : 'restaurants.score_ok',              
-                'score_sad'        : 'restaurants.score_sad',              
-                'price_range'      : 'restaurants.price_range',            
-                'district_id'      : 'districts.id',                       
-                'district_name'    : 'districts.name',                        
-                'region_name'      : 'districts.region' ,
-            })
-            .where('districts.name', district)
+                'id': 'restaurants.id',
+                'name': 'restaurants.name',
+                'telephone': 'restaurants.telephone',
+                'address': 'restaurants.address',
+                'latitude': 'restaurants.latitude',
+                'longitude': 'restaurants.longitude',
+                'img_url': 'restaurants.img_url',
+                'cuisine': 'restaurants.cuisine',
+                'category': 'restaurants.category',
+                'rating': 'restaurants.rating',
+                'score_smile': 'restaurants.score_smile',
+                'score_ok': 'restaurants.score_ok',
+                'score_sad': 'restaurants.score_sad',
+                'price_range': 'restaurants.price_range',
+                'district_id': 'districts.id',
+                'district_name': 'districts.name',
+                'region_name': 'districts.region',
+                'facility_id': 'restaurants_facilities.facility_id'
+                // })
+                //     .where('districts.name', district)
+                // }
+            });
+        if (districtName) {
+            query.where('districts.name', districtName);
         }
 
-    restauByFacility(district:string,facility:number){
+        if (facilityId) {
+            query.where('restaurants_facilities.facility_id', facilityId);
+        }
+        if (restaurantId) {
+            query.where('restaurant_id', restaurantId);
+        }
+        return query;
+    }
+
+    restauByFacility(district: string, facility: number) {
         return this.knex('restaurants')
-        .join('districts', 'districts.id', 'restaurants.district_id')
-        .join('restaurants_facilities', 'restaurants_facilities.restaurant_id', 'restaurants.id')
-        .join('facilities', 'facilities.id','restaurants_facilities.facility_id')
-        .select({
-            'id'               : 'restaurants.id',
-            'facility_id'      : 'restaurants_facilities.facility_id', 
-            'facility_name'    : 'facilities.description',     
-            'name'             : 'restaurants.name',                     
-            'telephone'        : 'restaurants.telephone',              
-            'address'          : 'restaurants.address',            
-            'latitude'         : 'restaurants.latitude',               
-            'longitude'        : 'restaurants.longitude',              
-            'img_url'          : 'restaurants.img_url',            
-            'cuisine'          : 'restaurants.cuisine',            
-            'category'         : 'restaurants.category',               
-            'rating'           : 'restaurants.rating',             
-            'score_smile'      : 'restaurants.score_smile',            
-            'score_ok'         : 'restaurants.score_ok',              
-            'score_sad'        : 'restaurants.score_sad',              
-            'price_range'      : 'restaurants.price_range',            
-            'district_id'      : 'districts.id',                       
-            'district_name'    : 'districts.name',                        
-            'region_name'      : 'districts.region' 
-        })
-        .where('districts.name', district)
-        .andWhere('restaurants_facilities.facility_id',facility)
+            .join('districts', 'districts.id', 'restaurants.district_id')
+            .join('restaurants_facilities', 'restaurants_facilities.restaurant_id', 'restaurants.id')
+            .join('facilities', 'facilities.id', 'restaurants_facilities.facility_id')
+            .select({
+                'id': 'restaurants.id',
+                'facility_id': 'restaurants_facilities.facility_id',
+                'facility_name': 'facilities.description',
+                'name': 'restaurants.name',
+                'telephone': 'restaurants.telephone',
+                'address': 'restaurants.address',
+                'latitude': 'restaurants.latitude',
+                'longitude': 'restaurants.longitude',
+                'img_url': 'restaurants.img_url',
+                'cuisine': 'restaurants.cuisine',
+                'category': 'restaurants.category',
+                'rating': 'restaurants.rating',
+                'score_smile': 'restaurants.score_smile',
+                'score_ok': 'restaurants.score_ok',
+                'score_sad': 'restaurants.score_sad',
+                'price_range': 'restaurants.price_range',
+                'district_id': 'districts.id',
+                'district_name': 'districts.name',
+                'region_name': 'districts.region'
+            })
+            .where('districts.name', district)
+            .andWhere('restaurants_facilities.facility_id', facility)
     }
-        
-    }
-    
+
+}
